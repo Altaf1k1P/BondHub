41f7351e5222da574ea46b916fe3570d0b6fbbc1
Altaf1k1 <altafpathan@gmail.com>
2025-01-05 17:36:55 +0530

    path

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { FriendRequest } from "../models/FriendRequest.model.js";
+import { FriendRequest } from "../models/friendRequest.model.js";
 import { FriendList } from "../models/friendList.model.js";
 import jwt from "jsonwebtoken";
 
